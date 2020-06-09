0d08add27aaa78496d0238b1403a66ee08e6e693
Anastasiia Gr <a.hrebenichenko@gmail.com>
2020-06-09 09:35:42 +0300

    first bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import settings
+
+logging.basicConfig(
+    format='%(asctime)s-%(levelname)s-%(message)s',
+    level=logging.INFO,
+    filename='bot.log'
+)
+
+def greet_user (bot, update):
+    text = ('Вызван старт/')
+    print (text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(update.message)
+    update.message.reply_text(user_text)
+
+
+def main():
+    mybot = Updater(settings.API)
+
+    logging.info('Bot live')
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle( )
+    
+
+    
+    
+main()
